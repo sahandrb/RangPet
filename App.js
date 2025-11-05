55475d9f00de7944e64fb124faf704d3367affd7
sahandrb <sahandrbp@gmail.com>
2025-11-05 14:10:28 +0330

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductPage from './pages/ProductPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import ChemicalPage from './pages/ChemicalPage';
+import LongevityPage from './pages/LongevityPage';
+import EffectivenessPage from './pages/EffectivenessPage';
+import NotFoundPage from './pages/NotFoundPage';
+import Layout from './components/Layout';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <BrowserRouter>
+      <Layout>
         <Routes>
           <Route path="/" element={<ProductPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/chemical" element={<ChemicalPage />} />
+          <Route path="/longevity" element={<LongevityPage />} />
+          <Route path="/effectiveness" element={<EffectivenessPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
